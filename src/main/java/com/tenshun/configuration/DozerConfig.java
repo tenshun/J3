9fa3a9c1980950c4f7b267a520bfdd11ad4a4f3a
rsadretdinov <rsadretdinov@i-novus.ru>
2016-10-13 15:46:44 +0300

    dozercfg

---
+++ src/main/java/com/tenshun/configuration/DozerConfig.java
@@ -0,0 +1,22 @@
+package com.tenshun.configuration;
+
+import org.dozer.spring.DozerBeanMapperFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import java.io.IOException;
+
+@Configuration
+public class DozerConfig {
+
+    @Bean
+    public DozerBeanMapperFactoryBean configDozer() throws IOException {
+        DozerBeanMapperFactoryBean mapper = new DozerBeanMapperFactoryBean();
+        Resource[] resources = new PathMatchingResourcePatternResolver().getResources("classpath*:dozer-bean-mappings.xml");
+        mapper.setMappingFiles(resources);
+        return mapper;
+    }
+
+}
